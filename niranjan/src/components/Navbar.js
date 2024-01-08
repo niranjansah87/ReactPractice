9fd7a789d27578668546add9b516c13a71b79d23
Niranjan <2100090187@kluniversity.in>
2024-01-08 18:13:16 +0530

    Props and PropsType

--- niranjan/src/components/Navbar.js
+++ niranjan/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
@@ -43,4 +44,10 @@ export default function Navbar(props) {
 </nav>
     </div>
   )
+}
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired
+};
+Navbar.defaultProps = {
+  title: "Niranjan"
 }
