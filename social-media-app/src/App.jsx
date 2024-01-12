82871357ca6c3abebcf09fce12511fba0b178a03
Niranjan <2100090187@kluniversity.in>
2024-01-12 22:33:53 +0530

    added some jsx file

--- social-media-app/src/App.jsx
+++ social-media-app/src/App.jsx
@@ -1,15 +1,24 @@
-
-
 import './App.css'
-import Header from './components/Header'
 import "bootstrap/dist/css/bootstrap.min.css"
+import Header from './components/Header'
+import "bootstrap/dist/css/bootstrap.css"
+import Footor from "./components/Footor";
+import Sidebar from './components/Sidebar';
+import CreatePost from './components/CreatePost';
+
 function App() {
   
 
   return (
     <>
+    <div className="app-container">
+    <Sidebar/>
+    <div className="content">
       <Header/>
-      
+      <CreatePost/>
+      <Footor/>
+      </div>
+      </div>
     </>
   )
 }
