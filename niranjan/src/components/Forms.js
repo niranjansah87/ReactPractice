18a13e60d864d61d09969df49b25ae989f2ecfcb
Niranjan <2100090187@kluniversity.in>
2024-01-13 19:11:56 +0530

    again started code with harry tuotorial

--- niranjan/src/components/Forms.js
+++ niranjan/src/components/Forms.js
@@ -1,30 +1,32 @@
-import React   from 'react'
+// import React   from 'react'
+import React, { useState } from 'react';
 export default function Forms() {
     // const [text, setText] = useState('Enter text here');
-    const handleSubmit=()=>{
+    const handleSubmit = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
         alert('Form Submitted');
     }
-
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+    const [text, setText] = useState('  Enter text here ');
     return (
         <>
             <div className="container">
-                <form action="/action_page.php">
-                    <div className="mb-3 mt-3">
-                        <label htmlFor="email" className="form-label">Email:</label>
-                        <input type="email" className="form-control" id="email" placeholder="Enter email" name="email" />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="pwd" className="form-label">Password:</label>
-                        <input type="password" className="form-control" id="pwd" placeholder="Enter password" name="pswd" />
-                    </div>
-                    <div className="form-check mb-3">
-                        <label className="form-check-label">
-                            <input className="form-check-input" type="checkbox" name="remember" /> Remember me
-                        </label>
-                    </div>
-                    <button type="submit" onClick={handleSubmit} className="btn btn-primary">Submit</button>
-                </form>
+                <h1>Niranjan</h1>
+                <div className="mb-3">
+                    <textarea value={text} onChange={handleOnChange} id="myBox" cols="30" rows="10" className="form-control"></textarea>
+                </div>
+
+                <button className="btn btn-primary" onClick={handleSubmit}>Convert to UpperCase</button>
             </div>
+            <div className="container my-2">
+            <h1>Your Text summary</h1>
+            <p>5552 words,5545 characters</p>
+
+            </div>
+
         </>
     )
 }
