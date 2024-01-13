fed12cfafa11221db16c79c371fb2e1d24aaef87
Niranjan <2100090187@kluniversity.in>
2024-01-13 19:19:31 +0530

    #8

--- niranjan/src/components/Forms.js
+++ niranjan/src/components/Forms.js
@@ -5,25 +5,32 @@ export default function Forms() {
     const handleSubmit = () => {
         let newText = text.toUpperCase();
         setText(newText);
+        // alert('Form Submitted');
+    }
+    const handleSubmit2 = () => {
+        let newText = text.toLowerCase();
+      
+        setText(newText);
         alert('Form Submitted');
     }
     const handleOnChange = (event) => {
         setText(event.target.value);
     }
-    const [text, setText] = useState('  Enter text here ');
+    const [text, setText] = useState('');
     return (
         <>
             <div className="container">
                 <h1>Niranjan</h1>
                 <div className="mb-3">
-                    <textarea value={text} onChange={handleOnChange} id="myBox" cols="30" rows="10" className="form-control"></textarea>
+                    <textarea  onChange={handleOnChange} id="myBox" cols="30" rows="10" className="form-control"></textarea>
                 </div>
 
                 <button className="btn btn-primary" onClick={handleSubmit}>Convert to UpperCase</button>
+                <button className="btn btn-primary mx-2" onClick={handleSubmit2}>Convert to Lowercase</button>
             </div>
             <div className="container my-2">
             <h1>Your Text summary</h1>
-            <p>5552 words,5545 characters</p>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
 
             </div>
 
