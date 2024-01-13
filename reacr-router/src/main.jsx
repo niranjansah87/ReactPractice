83826a323e8f33fa2c9d27be68d8038c3acc467a
Niranjan <2100090187@kluniversity.in>
2024-01-13 13:20:35 +0530

    something deleted

--- reacr-router/src/main.jsx
+++ reacr-router/src/main.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // import './index.css'
-
+// const router=createBrowserRouter([
+//   {path: '/', component: Home},
+//   {path: '/about', component: About},
+// ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
