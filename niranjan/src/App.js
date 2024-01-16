9deb0ec585e596c3780d0f1ab5494fb04505a080
Niranjan <2100090187@kluniversity.in>
2024-01-16 14:05:25 +0530

    #16 Learned React Router dom

--- niranjan/src/App.js
+++ niranjan/src/App.js
@@ -2,17 +2,36 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Forms from './components/Forms';
+// import alert from './components/alert';
 import About from './components/About'; // Import the 'About' component
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Navbar />
-      <Forms />
-      {/* <About/> */}
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path='/' element={<Forms />} />
+      </Routes>
+    </BrowserRouter>
     </>
-    
+
   );
 }
 
 export default App;
+
+// import React from 'react'
+
+// export default function App() {
+//   return (
+//    <>
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path='/' component={<Navbar />} />
+//       </Routes>
+//     </BrowserRouter>
+//    </>
+//   )
+// }
