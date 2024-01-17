92cf2668e081ee80baa54029410f526e06558d49
Niranjan <2100090187@kluniversity.in>
2024-01-17 22:15:20 +0530

    #44 created some schema and models

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -5,9 +5,13 @@ connectToMongoose();
 const app = express()
 const port = 3000
 
+//Available Routes
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
