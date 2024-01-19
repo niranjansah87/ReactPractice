c1f181b27960068203ba99dadb207207f76ffad6
Niranjan <2100090187@kluniversity.in>
2024-01-19 21:10:42 +0530

    #56

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -6,24 +6,27 @@ import About from './components/About'; // Import the 'About' component
 import Home from "./components/home"// Import the 'Home' component with correct casing
 // import NoteState from './components/NoteState'; // Import the 'NoteState' component
 import NoteState from './context/notes/NoteState';
+import Login from './components/Login'; // Import the 'Login' component
+import Signup from './components/Signup';
+import Alert from './components/Alert'; // Import the 'Alert' component
 function App() {
 
   return (
-     <>
-     <NoteState>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route index element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-  </NoteState>
+         <>
+         <NoteState>
+          <BrowserRouter>
+            <Navbar />
+            <Alert message="This is amazing React course" />
+            <Routes>
+              <Route exact path="/about" element={<About />} />
+              <Route index element={<Home />} />
+              <Route exact path="/login" element={<Login />} /> 
+              <Route exact path="/signup" element={<Signup />} /> 
+            </Routes>
+          </BrowserRouter>
+         </NoteState>
+         </>
 
-
-       
-      </>
-  
   );
 }
 
