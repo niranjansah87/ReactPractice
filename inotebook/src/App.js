c679db3c8f38e3202d3c370233cd7375d76ed406
Niranjan <2100090187@kluniversity.in>
2024-01-19 14:28:50 +0530

    Creating Navbar and Routes

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -1,11 +1,28 @@
 
 import './App.css';
+import {BrowserRouter ,Routes, Route} from "react-router-dom";
+import Navbar from './components/Navbar';
+import About from './components/About'; // Import the 'About' component
+import Home from "./components/home"// Import the 'Home' component with correct casing
 
 function App() {
+
   return (
    <>
-    
-   </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+
+
+
+
+     
+    </>
+  
   );
 }
 
